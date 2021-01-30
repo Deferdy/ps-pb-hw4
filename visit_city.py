a9a2959d1abb765fdffd6d9b37a69bb1425e1e2b
Deferdy <jourdain2012@gmail.com>
2021-01-30 13:42:18 +0300

    fourth commit

---
+++ visit_city.py
@@ -0,0 +1,13 @@
+cities = ['Москва', 'Париж', 'Лондон']
+users = [{'name': 'Иван', 'age': 35 }, {'name': 'Мария', 'age': 22}, {'name': 'Соня', 'age': 20}]
+tourists = [{'user': users[0], 'city': cities[2]}, {'user': users[1], 'city': cities[0]}, {'user': users[2], 'city': cities[1]}]
+city = input('Введите город: ')
+
+if city.lower() == tourists[0]['city'].lower(): 
+    print('Турист ' + tourists[0]["user"]["name"] + ' возраст ' + str(tourists[0]["user"]['age']) + " .Посетил город " + tourists[0]["city"]) 
+elif city.lower() == tourists[1]["city"].lower():  
+    print('Турист ' + tourists[1]["user"]["name"] + ' возраст ' + str(tourists[1]["user"]['age']) + " .Посетил город " + tourists[1]["city"]) 
+elif city.lower() == tourists[2]["city"].lower():  
+    print('Турист ' + tourists[2]["user"]["name"] + ' возраст ' + str(tourists[2]["user"]['age']) + " .Посетил город " + tourists[2]["city"]) 
+
+ 
